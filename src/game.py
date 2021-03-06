3ac7e554a18523888f70d7c2009e6af0d57f11c5
Beatriz Mendes <45185351+biam05@users.noreply.github.com>
2021-03-06 20:24:04 +0000

    pygame works

--- src/game.py
+++ src/game.py
@@ -1,31 +1,19 @@
-# Simple pygame program
+import pygame, sys
+from pygame.locals import *
 
-# Import and initialize the pygame library
-import pygame
 pygame.init()
 
-# Set up the drawing window
-screen = pygame.display.set_mode([500, 500])
+DISPLAYSURF = pygame.display.set_mode((256, 256), 0, 32)
 
-image = pygame.image.load_basic('./src/maybe.bmp')
+catImg = pygame.image.load('cat.png')
 
-# Run until the user asks to quit
-running = True
-while running:
+while True:
+    DISPLAYSURF.fill((255, 255, 255))
 
-    # Did the user click the window close button?
+    DISPLAYSURF.blit(catImg, (0, 0))
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
 
-    # Fill the background with white
-    screen.fill((255, 255, 255))
-
-    # Draw a solid blue circle in the center
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
-
-    # Flip the display
-    pygame.display.flip()
-
-# Done! Time to quit.
-pygame.quit()
+    pygame.display.update()
