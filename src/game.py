5fdf9003975171522d1efd41beee99dfd3a934bc
Telmooo <telmoabaptista@hotmail.com>
2021-03-16 11:50:38 +0000

    Basic operation of game

--- src/game.py
+++ src/game.py
@@ -1,19 +1,54 @@
 import pygame, sys
 from pygame.locals import *
+from game_state import GameState
 
-pygame.init()
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
 
-DISPLAYSURF = pygame.display.set_mode((256, 256), 0, 32)
+test_state = [
+    [1, 1, 0, 1],
+    [0, 1, 0, [2, "purple"]],
+    [[3, "purple"], 1, [2, "purple"], 1],
+    [0, 0, 0, [3, "purple"]]
+]
 
-catImg = pygame.image.load('cat.png')
+game_state = GameState(test_state)
 
-while True:
-    DISPLAYSURF.fill((255, 255, 255))
+print(game_state)
 
-    DISPLAYSURF.blit(catImg, (0, 0))
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
+game_state.swipe_left()
 
-    pygame.display.update()
+print(game_state)
+
+"""
+def grid(block_size, centered=False):
+    real_block_size = block_size+1
+    width = WINDOW_WIDTH // real_block_size
+    height = WINDOW_HEIGHT // real_block_size
+    offset_x = 0
+    offset_y = 0
+    if (centered):
+        offset_x = (SCREEN.get_width() - real_block_size*width)//2
+        offset_y = (SCREEN.get_height() - real_block_size*height)//2
+
+    for x in range(width):
+        for y in range(height):
+            rect = pygame.Rect(offset_x + x * real_block_size, offset_y + y * real_block_size, block_size, block_size)
+            pygame.draw.rect(SCREEN, (200, 200, 200), rect, 1)
+
+def main():
+    global SCREEN
+    pygame.init()
+    SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    pygame.display.set_caption('Match-the-Tiles')
+
+    while True:
+        grid(32, True)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                return
+
+        pygame.display.flip()
+
+if __name__ == '__main__': main()
+"""
---
+++ src/game_state.py
@@ -0,0 +1,60 @@
+class GameState:
+    EMPTY = 0
+    WALL = 1
+    BLOCK = 2
+    GOAL = 3
+
+    def __init__(self, matrix):
+        self.matrix = matrix
+        self.rows = len(matrix)
+        self.cols = len(matrix[0])
+
+        self.walls = []
+        self.goals = []
+        self.blocks = []
+
+        for i in range(self.rows):
+            for j in range(self.cols):
+                obj = matrix[i][j]
+                if (obj == self.WALL):
+                    self.walls.append((i, j))
+                elif (isinstance(obj, list)):
+                    if (obj[0] == self.BLOCK):
+                        self.blocks.append([i, j])
+                    elif (obj[0] == self.GOAL):
+                        self.goals.append((i, j))
+
+    def move(self, pos, new_pos):
+        obj = self.matrix[pos[0]][pos[1]]
+        self.matrix[pos[0]][pos[1]] = self.EMPTY
+        self.matrix[new_pos[0]][new_pos[1]] = obj 
+
+    def swipe_left(self):
+        self.blocks.sort(key=lambda el: el[1])
+        for i in range(len(self.blocks)):
+            block = self.blocks[i]
+            walls = sorted(filter(lambda el: el[0] == block[0] and el[1] < block[1], self.walls + self.blocks[:i]), key=lambda el: -el[1])
+            print(walls)
+            new_col = walls[0][1] + 1 if walls else 0
+            print(new_col)
+            self.move(block, (block[0], new_col))
+            self.blocks[i][1] = new_col
+
+    def __repr__(self):
+        out = "["
+        for i in range(self.rows):
+            out += "\n["
+            for j in range(self.cols):
+                obj = self.matrix[i][j]
+                if (obj == self.EMPTY):
+                    out += " EMPTY\t\t"
+                if (obj == self.WALL):
+                    out += " WALL\t\t"
+                elif (isinstance(obj, list)):
+                    if (obj[0] == self.BLOCK):
+                        out += f" BLOCK[{obj[1]}]\t\t"
+                    elif (obj[0] == self.GOAL):
+                        out += f" GOAL[{obj[1]}]\t\t"
+            out += "\t]"
+        out += "\n]\n"
+        return out
