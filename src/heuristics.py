a8154da0b1ab210bc2b69407294c95d27c4e9730
Beatriz Mendes <45185351+biam05@users.noreply.github.com>
2021-03-20 20:06:01 +0000

    1st try with euclidean distance

--- src/heuristics.py
+++ src/heuristics.py
@@ -1,4 +1,5 @@
 from game_state import *
+import math
 
 """
 Calculate the euclidean distance between blocks and goals
@@ -9,8 +10,17 @@ Calculate the euclidean distance between blocks and goals
 def euclidean_distance(game_state, dist_func, node_func):
     dists = []
     for block in game_state.blocks:
+        color_block = game_state.matrix[block[0]][block[1]].block # isto deve estar tudo mal
         node_dists = []
         for goal in game_state.goals:
+            color_goal = game_state.matrix[goal[0]][goal[1]].goal # isto deve estar tudo mal
+            if color_block == color_goal:
+                # d = sqrt((x2-x1)**2 + (y2-y1)**2)
+                dnode = math.sqrt(math.pow((block[0]-goal[0]),2)+math.pow((block[1]-goal[1]),2))
+                node_dists.append(dnode)
+        d = node_func(node_dists)
+        dists.append(d)
+    return dist_func(dists)
 
 """
 Calculate the manhattan distance between blocks and goals
