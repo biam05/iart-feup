d585051c8f8e78bccf0e65c95864651d63335b3c
Beatriz Mendes <45185351+biam05@users.noreply.github.com>
2021-03-29 19:01:18 +0100

    reader of file

--- src/match_the_tiles/logic/reader.py
+++ src/match_the_tiles/logic/reader.py
@@ -5,6 +5,30 @@ def get_level(level_no):
     game_state = read_file(file_name)
 
 def read_file(file_name):
+
     file = open(file_name, 'r')
 
     lines = file.readlines()
+
+    x = 0
+    y = 0
+    rows = lines.len()
+    cols = lines[0].len()
+    walls = []
+    goals = []
+    blocks = []
+    for line in lines:
+        for char in line:
+            if char == '#':
+                walls.append([x, y])
+            elif char.isalpha():
+                if char.isupper():
+                    goals.append([x, y, char])
+                else:
+                    blocks.append([x, y, char])                
+            y += 1
+        x += 1
+        y = 0
+
+    commonGameState = CommonGameState(walls, goals, rows, cols)
+    return GameState(commonGameState, blocks)
