2beb755327ce402fe0dd0f2cf4866de145468297
Nuno Santos <up201405774@fe.up.pt>
2021-04-03 04:19:56 +0100

    working heuristic

--- src/match_the_tiles/logic/game_state.py
+++ src/match_the_tiles/logic/game_state.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 
 from match_the_tiles.model.tile_data import WallData, BlockData, GoalData
 from utils.utils import Coords
+from utils.utils import CoordswithColor
 import itertools
 
 from enum import Enum
@@ -38,73 +39,86 @@ class CommonGameState:
 
         self.rows = rows
         self.cols = cols
+        self.proccessdict={}
+        self.allWalls=walls
 
     #starts here
-    def get_goal_surrouding_walls(self,rows,cols,walls,goals):
-        goal=goals[0]
-        extra_walls=[]
+    def addExtraWalls(self,walls,cols,rows):
         i=0
+        extrawalls=walls
+        while i!=cols:
+            extrawalls.extend([[-1,i],[cols,i]])
+            extrawalls.extend([[i,-1],[i,rows]])
+            i=i+1
+        return extrawalls
+
+    def get_goal_surrouding_walls(self,rows,cols,defaultwalls,goals):
         counter=1
         finaldict={}
-        while i!=cols:
-            walls.extend([[-1,i],[cols,i]])
-            walls.extend([[i,-1],[i,rows]])
-            i=i+1
-
-        for wall in walls:
-            coord=Coords(wall[0],wall[1])
-            finaldict[coord]=[sys.maxsize,sys.maxsize,sys.maxsize,sys.maxsize]
-
-        obstacleN = list(filter(lambda el: el[0] == goal[0]-1 and el[1] == goal[1], walls))
-        if len(obstacleN):
-            finaldict = self.updateDict(0,finaldict,0,obstacleN[0][0],obstacleN[0][1])
-
-        obstacleS = list(filter(lambda el: el[0] == goal[0]+1 and el[1] == goal[1], walls))
-        if len(obstacleS):
-            finaldict = self.updateDict(1,finaldict,0,obstacleS[0][0],obstacleS[0][1])
-
-        obstacleW = list(filter(lambda el: el[1] == goal[1]-1 and el[0] == goal[0], walls))
-        if len(obstacleW):
-            finaldict = self.updateDict(2,finaldict,0,obstacleW[0][0],obstacleW[0][1])
-
-        obstacleE = list(filter(lambda el: el[1] == goal[1]+1 and el[0] == goal[0], walls))
-        if len(obstacleE):
-            finaldict = self.updateDict(3,finaldict,0,obstacleE[0][0],obstacleE[0][1])
-
         
         
-        obstacles=[obstacleN,obstacleS,obstacleW,obstacleE]
-        result=self.insert_vips(goals, walls, rows, cols,obstacles,finaldict,counter)
+        for goal in goals:
+            walls=self.addExtraWalls(defaultwalls,cols,rows)
+            for wall in walls:
+                coord=CoordswithColor(wall[0],wall[1],goal[2])
+                finaldict[coord]=[sys.maxsize,sys.maxsize,sys.maxsize,sys.maxsize]
+
+            obstacleN = list(filter(lambda el: el[0] == goal[0]-1 and el[1] == goal[1], walls))
+            if len(obstacleN):
+                finaldict = self.updateDict(1,finaldict,0,obstacleN[0][0],obstacleN[0][1],goal[2])
+                walls=[x for x in walls if x not in obstacleN]
+
+            obstacleS = list(filter(lambda el: el[0] == goal[0]+1 and el[1] == goal[1], walls))
+            if len(obstacleS):
+                finaldict = self.updateDict(0,finaldict,0,obstacleS[0][0],obstacleS[0][1],goal[2])
+                walls=[x for x in walls if x not in obstacleS]
+
+            obstacleW = list(filter(lambda el: el[1] == goal[1]-1 and el[0] == goal[0], walls))
+            if len(obstacleW):
+                finaldict = self.updateDict(2,finaldict,0,obstacleW[0][0],obstacleW[0][1],goal[2])
+                walls=[x for x in walls if x not in obstacleW]
 
 
+            obstacleE = list(filter(lambda el: el[1] == goal[1]+1 and el[0] == goal[0], walls))
+            if len(obstacleE):
+                finaldict = self.updateDict(3,finaldict,0,obstacleE[0][0],obstacleE[0][1],goal[2])
+                walls=[x for x in walls if x not in obstacleE]
 
-    def insert_vips(self,goals, walls, rows, cols,obstacles, finaldict,counter):
+            
+            
+            obstacles=[obstacleN,obstacleS,obstacleW,obstacleE]
+            finaldict=self.insert_vips(goal, walls, rows, cols,obstacles,finaldict,counter)
+        self.proccessdict=finaldict
+        self.allWalls=self.addExtraWalls(defaultwalls,cols,rows)
+        
+
+    def insert_vips(self,goal, walls, rows, cols,obstacles, finaldict,counter):
         new_E_walls=[]
         new_W_walls=[]
         new_N_walls=[]
         new_S_walls=[]
         
         for wall in obstacles[0]:#N
-            E_walls=list(filter(lambda el: el[1] == wall[1]+1 and el[0] > wall[0] and self.is_it_a_plane_is_it_a_bird_no_its_a_wall(walls,el[0],wall[1]), walls))#check for wall
-            W_walls=list(filter(lambda el: el[1] == wall[1]-1 and el[0] > wall[0] and self.is_it_a_plane_is_it_a_bird_no_its_a_wall(walls,el[0],wall[1]), walls))
+            E_walls=list(filter(lambda el: el[1] == wall[1]+1 and el[0] > wall[0] and self.is_it_a_plane_is_it_a_bird_no_its_a_wall(2,walls,el,wall), walls))#0 for horizontal right, 1 for horizontal left, 2 for vertical up, 3 for vertical down
+            W_walls=list(filter(lambda el: el[1] == wall[1]-1 and el[0] > wall[0] and self.is_it_a_plane_is_it_a_bird_no_its_a_wall(2,walls,el,wall), walls))
             new_E_walls.extend(E_walls)
             new_W_walls.extend(W_walls)
             
         for wall in obstacles[1]:#S
-            E_walls=list(filter(lambda el: el[1] == wall[1]+1 and el[0] < wall[0] and self.is_it_a_plane_is_it_a_bird_no_its_a_wall(walls,el[0],wall[1]), walls))
-            W_walls=list(filter(lambda el: el[1] == wall[1]-1 and el[0] < wall[0] and self.is_it_a_plane_is_it_a_bird_no_its_a_wall(walls,el[0],wall[1]), walls))
+            E_walls=list(filter(lambda el: el[1] == wall[1]+1 and el[0] < wall[0] and self.is_it_a_plane_is_it_a_bird_no_its_a_wall(3,walls,el,wall), walls))
+            W_walls=list(filter(lambda el: el[1] == wall[1]-1 and el[0] < wall[0] and self.is_it_a_plane_is_it_a_bird_no_its_a_wall(3,walls,el,wall), walls))
             new_E_walls.extend(E_walls)
             new_W_walls.extend(W_walls)
 
         for wall in obstacles[2]:#W
-            N_walls=list(filter(lambda el: el[0] == wall[0]-1 and el[1] > wall[1] and self.is_it_a_plane_is_it_a_bird_no_its_a_wall(walls,wall[0],el[1]), walls))
-            S_walls=list(filter(lambda el: el[0] == wall[0]+1 and el[1] > wall[1] and self.is_it_a_plane_is_it_a_bird_no_its_a_wall(walls,wall[0],el[1]), walls))
+            N_walls=list(filter(lambda el: el[0] == wall[0]-1 and el[1] > wall[1] and self.is_it_a_plane_is_it_a_bird_no_its_a_wall(0,walls,wall,el), walls))
+            S_walls=list(filter(lambda el: el[0] == wall[0]+1 and el[1] > wall[1] and self.is_it_a_plane_is_it_a_bird_no_its_a_wall(0,walls,wall,el), walls))
             new_N_walls.extend(N_walls)
             new_S_walls.extend(S_walls)
 
         for wall in obstacles[3]:#E
-            N_walls=list(filter(lambda el: el[0] == wall[0]-1 and el[1] < wall[1] and self.is_it_a_plane_is_it_a_bird_no_its_a_wall(walls,wall[0],el[1]), walls))
-            S_walls=list(filter(lambda el: el[0] == wall[0]+1 and el[1] < wall[1] and self.is_it_a_plane_is_it_a_bird_no_its_a_wall(walls,wall[0],el[1]), walls))
+            N_walls=list(filter(lambda el: el[0] == wall[0]-1 and el[1] < wall[1] and self.is_it_a_plane_is_it_a_bird_no_its_a_wall(1,walls,wall,el), walls))
+            S_walls=list(filter(lambda el: el[0] == wall[0]+1 and el[1] < wall[1] and self.is_it_a_plane_is_it_a_bird_no_its_a_wall(1,walls,wall,el), walls))
             new_N_walls.extend(N_walls)
             new_S_walls.extend(S_walls)
 
@@ -117,16 +131,16 @@ class CommonGameState:
         new_W_walls.sort()
         new_W_walls=list(k for k,_ in itertools.groupby(new_W_walls))
         for wall in new_N_walls:
-            finaldict = self.updateDict(0,finaldict,counter,wall[0],wall[1])
+            finaldict = self.updateDict(1,finaldict,counter,wall[0],wall[1],goal[2])
 
         for wall in new_S_walls:
-            finaldict = self.updateDict(1,finaldict,counter,wall[0],wall[1])
+            finaldict = self.updateDict(0,finaldict,counter,wall[0],wall[1],goal[2])
 
         for wall in new_E_walls:
-            finaldict = self.updateDict(2,finaldict,counter,wall[0],wall[1])
+            finaldict = self.updateDict(3,finaldict,counter,wall[0],wall[1],goal[2])
 
         for wall in new_W_walls:
-            finaldict = self.updateDict(3,finaldict,counter,wall[0],wall[1])
+            finaldict = self.updateDict(2,finaldict,counter,wall[0],wall[1],goal[2])
             
 
         counter=counter+1
@@ -139,20 +153,34 @@ class CommonGameState:
         if((not new_N_walls) and (not new_S_walls) and (not new_E_walls) and (not new_W_walls)):
             return finaldict
         
-        result=self.insert_vips(goals, walls, rows, cols,obstacles,finaldict,counter)
+        result=self.insert_vips(goal, walls, rows, cols,obstacles,finaldict,counter)
         return result
     
-    def updateDict(self,orientation,finaldict,counter,x,y):
-        coord=Coords(x,y)
+    def updateDict(self,orientation,finaldict,counter,x,y,goal):
+        coord=CoordswithColor(x,y,goal)
         previous=finaldict.get(coord)
         previous[orientation]=counter
         finaldict.update({coord:previous})
         return finaldict
 
-    def is_it_a_plane_is_it_a_bird_no_its_a_wall(self,walls,x,y):
+    def is_it_a_plane_is_it_a_bird_no_its_a_wall(self,orientation,walls,element,chosenwall):
         for wall in walls:
-            if(x==wall[0] and y==wall[1]):
-                return False
+            if orientation==0:
+                if(wall[1]>chosenwall[1] and wall[0]==chosenwall[0]):
+                    if(element[1]>=wall[1]):
+                        return False
+            if orientation==1:
+                if(wall[1]<chosenwall[1] and wall[0]==chosenwall[0]):
+                    if(element[1]<=wall[1]):
+                        return False
+            if orientation==2:
+                if(wall[0]>chosenwall[0] and wall[1]==chosenwall[1]):
+                    if(element[0]>=wall[0]):
+                        return False
+            if orientation==3:
+                if(wall[0]<chosenwall[0] and wall[1]==chosenwall[1]):
+                    if(element[0]<=wall[0]):
+                        return False
         return True
     #ends here
 
@@ -281,35 +309,64 @@ class GameState:
             colinear_goals = filter(lambda el: el.color.upper() == block.color.upper() and (el.coords.x == block.coords.x or el.coords.y == block.coords.y), self.common_gs.goals)
             # colinear
             if colinear_goals:
-                print(0)
                 for goal in colinear_goals:
                     goal_x = goal.coords.x
                     goal_y = goal.coords.y
                     if goal_x == block_x and goal_y == block_y:
                         local_moves = min(local_moves, 0)
-                        print(1)
                         break
                     obstacles = filter(lambda el: el != block and el.in_between(goal, block), self.blocks + self.common_gs.walls)
                     # has an obstacle between the block and the goal
                     if obstacles:
                         local_moves = min(local_moves, 3)
-                        print(2)
                     else:
                         print(block_x, block_y, goal_x, goal_y)
                         if (goal_x == block_x and (goal_y == 0 or goal_y == self.common_gs.cols - 1)) or (goal_y == block_y and (goal_x == 0 or goal_x == self.common_gs.rows - 1)):
                             local_moves = min(local_moves, 1)
-                            print(3)
                         else:
                             obstacles = filter(lambda el: self.is_wall_stopping_block_at_goal(block, goal, el), self.common_gs.walls)
                             local_moves = min(local_moves, 1 if obstacles else 2)
-                            print(4)
             # non-colinear
             else:
                 local_moves = min(local_moves, 2)
-                print(5)
 
             moves = max(moves, local_moves)
         return moves
+
+
+    def choose_path(self):
+        value=[sys.maxsize,sys.maxsize,sys.maxsize,sys.maxsize]
+        walls=self.common_gs.allWalls
+        for block in self.blocks:
+            #Check if block to the north
+            wallTOnorth=next(filter(lambda el: el[0] == block.coords.x-1 and el[1] == block.coords.y,walls),False)
+            if wallTOnorth:
+                coord=CoordswithColor(wallTOnorth[0],wallTOnorth[1],block.color.upper())
+                allValues=self.common_gs.proccessdict.get(coord)
+                value[0]=min(value[0],allValues[1])
+                
+
+            #Check if block to the south
+            wallTOsouth=next(filter(lambda el: el[0] == block.coords.x+1 and el[1] == block.coords.y,walls),False)
+            if wallTOsouth:
+                coord=CoordswithColor(wallTOsouth[0],wallTOsouth[1],block.color.upper())
+                allValues=self.common_gs.proccessdict.get(coord)
+                value[1]=min(value[1],allValues[0])
+
+            #Check if block to the east
+            wallTOeast=next(filter(lambda el: el[0] == block.coords.x and el[1] == block.coords.y+1,walls),False)
+            if wallTOeast:
+                coord=CoordswithColor(wallTOeast[0],wallTOeast[1],block.color.upper())
+                allValues=self.common_gs.proccessdict.get(coord)
+                value[2]=min(value[2],allValues[3])
+
+            #Check if block to the west
+            wallTOwest=next(filter(lambda el: el[0] == block.coords.x and el[1] == block.coords.y-1,walls),False)
+            if wallTOwest:
+                coord=CoordswithColor(wallTOwest[0],wallTOwest[1],block.color.upper())
+                allValues=self.common_gs.proccessdict.get(coord)
+                value[3]=min(value[3],allValues[2])
+        return min(value)
 
     """
     Swipe Left Operation - Moves the movable blocks in the GameState to the left
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -45,3 +45,51 @@ class Coords:
 
     def __hash__(self):
         return hash((self.x, self.y))
+
+
+
+@dataclass
+class CoordswithColor:
+    """
+    - x : Row value
+    - y : Column value
+    """
+    x: int
+    y: int
+    c: str
+
+    """
+    Checks if two Coords are equal
+    """
+
+    def __eq__(self, other):
+        return (self.x == other.x) and (self.y == other.y)
+
+    """
+    X Setter
+    """
+
+    def setX(self, x):
+        self.x = x
+
+    """
+    Y Setter
+    """
+
+    def setY(self, y):
+        self.y = y
+
+    """
+    Checks if Coords is in between
+    """
+
+    def in_between(self, c1, c2):
+        if self.x == c1.x and self.x == c2.x:
+            return (self.y > c1.y and self.y < c2.y) or (self.y < c1.y and self.y > c2.y)
+        if self.y == c1.y and self.y == c2.y:
+            return (self.x > c1.x and self.x < c2.x) or (self.x < c1.x and self.x > c2.x)
+        return False
+
+
+    def __hash__(self):
+        return hash((self.x, self.y,self.c))
