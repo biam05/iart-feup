87038291c573931310504a8399e9f761bef202cd
Telmooo <telmoabaptista@hotmail.com>
2021-04-03 18:03:16 +0100

    Algorithm Statistics

--- src/statistics.py
+++ src/statistics.py
@@ -1,13 +1,31 @@
 import sys
 
 from solver import solver
+from solver import algorithms
+import numpy as np
 
-def run_algorithm(levels, advanced, algorithm):
-    levels_sufix = "adv" if advanced else "normal"
-    sys.stdout = open(algorithm + levels_sufix + ".csv", "w")
+N_TRIALS = 20
 
-    print("Level, Expanded Nodes, Number of Moves, Moves")
+def run_algorithm(levels, algorithm):
+    print(algorithm)
+    print(f"Level, Expanded Nodes ({algorithm}), Time ({algorithm}), Number of Moves ({algorithm}), Moves")
     for level in levels:
-        _, time, solution, nMoves, _, _, expanded_nodes = solver(level, advanced, algorithm)
+        n_level = level - 20 if level > 20 else level
+        level_suffix = "A" if level > 20 else ""
+        times = []
+        solution = []
+        nMoves = -1
+        expanded_nodes = -1
+        for _ in range(N_TRIALS):
+            _, time, solution, nMoves, _, _, expanded_nodes = solver(n_level, level > 20, algorithm)
+            times.append(time)
 
-        print()
+        print(", ".join(map(str, [str(n_level) + level_suffix, expanded_nodes, np.mean(times), nMoves, solution])))
+
+def run_algorithms():
+    sys.stdout = open("statistics/algorithms.csv", "w+")
+    for alg in algorithms.keys():
+        run_algorithm(range(1, 43), alg)
+
+if __name__ == "__main__":
+    run_algorithms()
